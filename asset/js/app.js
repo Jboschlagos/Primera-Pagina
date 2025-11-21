86fa377861c000474c82b4dfa085adc2b6168d79
jboschlagos <jboschlagos@gmail.com>
2025-11-20 22:21:35 -0300

    estaba mal ejecutado el link de Css, corregido

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,12 +1,8 @@
-// 1. Buscar el botón en el HTML
 const botonArriba = document.querySelector('#ir-arriba');
 
-// 2. Escuchar cuando el usuario hace clic
 botonArriba.addEventListener('click', () => {
-  
-  // 3. Mover la página hacia arriba con scroll suave
   window.scrollTo({
-    top: 0,          // hasta arriba
-    behavior: 'smooth'  // suavemente
+    top: 0,
+    behavior: 'smooth'
   });
 });
